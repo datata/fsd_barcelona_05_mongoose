3b5fc1f3c52f1a0b335d5dcd2b015e27f4a945d8
Dani <d.tarazoma@geekshubs.com>
2022-06-20 12:45:55 +0200

    feat: mongoose connection

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 //routes
 app.get('/', (req, res) =>{
     return res.send('Bienevenidos a mi aplicacion de tareas');
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+.then(() => {
+    console.log('Connection stablished');
+})
+.catch((error) => {
+    console.log('Error connecting to MongoDB', error);
 });
 
 app.listen(3000, () => {
