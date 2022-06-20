5d64dd74180f61b64947bcec798217514bc7dec1
Dani <d.tarazoma@geekshubs.com>
2022-06-20 13:31:09 +0200

    feat: refactor connection mongoose

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config();
+const db = require('./config/database');
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
 //routes
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     return res.send('Bienevenidos a mi aplicacion de tareas');
 });
 
-//connection to mongodb
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log('Connection stablished');
-})
-.catch((error) => {
-    console.log('Error connecting to MongoDB', error);
-});
-
-app.listen(port, () => {
-    console.log('Server is running: ' + port);
-});
+db()
+    .then(() => {
+        app.listen(port, () => {
+            console.log('Server is running: ' + port);
+        });
+    })
+    .catch((error) => {
+        console.log("Error Connecting to mongoDB", error);
+    });
