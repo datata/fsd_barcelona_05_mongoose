9656d3701a97217c2e0a713514e0cd682c1d1a66
Dani <d.tarazoma@geekshubs.com>
2022-06-21 11:14:27 +0200

    feat: getAll users with logic

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express');
 require('dotenv').config();
 const db = require('./config/database');
+const User = require('./models/User');
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
 //routes
-app.get('/users', (req, res) => {
-    return res.status(200).json(
-        {
-            success: true,
-            message: 'Get all users retrieved succsessfully'
-        }
-    );
-})
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+
+        return res.status(200).json(
+            {
+                success: true,
+                message: 'Get all users retrieved succsessfully',
+                data: users
+            }
+        );
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: 'Error retrieving users',
+                error: error.message
+            }
+        )
+    }
+});
 
 
 app.get('/', (req, res) => {
