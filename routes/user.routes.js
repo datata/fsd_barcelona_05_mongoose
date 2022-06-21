cca82337aaf37909852ab4870f26b4df5171a5c7
Dani <d.tarazoma@geekshubs.com>
2022-06-21 13:09:43 +0200

    feat: getAll users refactor logic to user Controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,28 +1,9 @@
 const User = require('../models/User');
 
 const router = require('express').Router();
+const userController = require('../controllers/UserController');
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-
-        return res.status(200).json(
-            {
-                success: true,
-                message: 'Get all users retrieved succsessfully',
-                data: users
-            }
-        );
-    } catch (error) {
-        return res.status(500).json(
-            {
-                success: false,
-                message: 'Error retrieving users',
-                error: error.message
-            }
-        )
-    }
-});
+router.get('/users', userController.getAll);
 
 router.post('/users', async (req, res) => {
     try {
