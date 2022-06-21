5c2507b09b8b961fb117e745c9f05e6aa2776ced
Dani <d.tarazoma@geekshubs.com>
2022-06-21 12:48:44 +0200

    feat: refactor findAll users to users routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const db = require('./config/database');
 const User = require('./models/User');
+const userRoutes = require('./routes/user.routes')
 
 const app = express();
 
@@ -11,27 +12,7 @@ app.use(express.json());
 const port = process.env.PORT || 4000;
 
 //routes
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-
-        return res.status(200).json(
-            {
-                success: true,
-                message: 'Get all users retrieved succsessfully',
-                data: users
-            }
-        );
-    } catch (error) {
-        return res.status(500).json(
-            {
-                success: false,
-                message: 'Error retrieving users',
-                error: error.message
-            }
-        )
-    }
-});
+app.use('/api', userRoutes);
 
 app.post('/users', async (req, res) => {
     try {
